d97c7e8d0d626b40471a6ac7954bd55aa671b4c2
Alexander Schrijver <alexander.schrijver@gmail.com>
2011-01-05 20:37:43 +0100

    Add plural description. Remove some superflous comments

--- web/models.py
+++ web/models.py
@@ -2,60 +2,60 @@ from django.db import models
 
 class Betrokkene(models.Model):
     naam = models.CharField(max_length=255)
-
     melding = models.ForeignKey('Melding')
 
     def __unicode__(self):
         return self.naam
 
+    class Meta:
+        verbose_name=('Betrokkene')
+        verbose_name_plural=('Betrokkenen')
+
+
 class BetrokkeneData(models.Model):
     name = models.CharField(max_length=255)
     value = models.CharField(max_length=255)
-
     betrokkene = models.ForeignKey('Betrokkene')
 
     def __unicode__(self):
         return self.name
 
-#class Company(models.Model):
-#    url = models.CharField(max_length=255)
-#    name = models.CharField(max_length=255)
-#
-#    def __unicode__(self):
-#        return self.name
+    class Meta:
+        verbose_name=('Betrokkene data')
+        verbose_name_plural=('Betrokkenen data')
 
 class Doel(models.Model):
     naam = models.CharField(max_length=255)
-
     melding = models.ForeignKey('Melding')
 
     def __unicode__(self):
         return self.naam
 
+    class Meta:
+        verbose_name=('Doel')
+        verbose_name_plural=('Doelen')
+
 class Melding(models.Model):
-#    cbp_id = models.BigIntegerField()
     description = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
-
-#    company = models.ForeignKey('Company')
 
     def __unicode__(self):
         return "%d" % (self.id, )
 
+    class Meta:
+        verbose_name=('Melding')
+        verbose_name_plural=('Meldingen')
+
 class Ontvanger(models.Model):
     naam = models.CharField(max_length=255)
-
     melding = models.ForeignKey('Melding')
 
     def __unicode__(self):
         return self.naam
 
-#class Verantwoordelijke(models.Model):
-#    bezoekadres = models.CharField(max_length=255)
-#    naam = models.CharField(max_length=255)
-#    postadres = models.CharField(max_length=255)
-#
-#    melding = models.ForeignKey('Melding')
+    class Meta:
+        verbose_name=('Ontvanger')
+        verbose_name_plural=('Ontvangers')
 
 class Company(models.Model):
     bezoekadres = models.CharField(max_length=255)
@@ -66,3 +66,7 @@ class Company(models.Model):
 
     def __unicode__(self):
         return self.naam
+
+    class Meta:
+        verbose_name=('Organisatie')
+        verbose_name_plural=('Organisaties')
